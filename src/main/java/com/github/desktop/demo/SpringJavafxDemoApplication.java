773edf77d0b9c931b2b34ef506ca45053886934f
mrdafian47 <mohamad.rizki.dafianto@gmail.com>
2025-01-06 10:06:35 +0700

    init: initial commit

---
+++ src/main/java/com/github/desktop/demo/SpringJavafxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.desktop.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavafxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavafxDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/desktop/demo/SpringJavafxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.desktop.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavafxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
